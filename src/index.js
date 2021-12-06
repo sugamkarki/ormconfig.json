f7de4971330f6b3ac83ad9e92793f449119cdc3d
magusikrak <sugamkarki7058@gmail.com>
2021-12-06 14:09:37 +0545

    converted json to yml

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const exec = require("child_process").exec;
 const { writeFile, readdir, readFile } = require("fs").promises;
 const inquirer = require("inquirer");
 const path = require("path");
-const YAML = require("json-to-pretty-yaml");
+const YAML = require("js-yaml");
 //
 const configFiles = {};
 const configFolderPath = path.resolve(__dirname, "config");
@@ -34,16 +34,18 @@ const configFolderPath = path.resolve(__dirname, "config");
     },
   ]);
   let config = await readFile(configFiles[database]).catch(console.log);
-  let ormconfig = path.join(process.cwd(), "ormconfig.json");
-  await writeFile(ormconfig, config.toString()).catch((err) => {
-    console.log(err);
-    process.exit();
-  });
+  const json = config.toString();
+
+  let ormconfig = path.join(process.cwd(), `ormconfig.${extension}`);
+  if (extension === "json") {
+    await writeFile(ormconfig, json).catch((err) => {
+      console.log(err);
+      process.exit();
+    });
+  }
   if (extension === "yml") {
-    let ormconfig = path.join(process.cwd(), "ormconfig.yml");
-    const json = require(path.join(__dirname, "../ormconfig.json"));
-    const data = YAML.stringify(json);
-    await writeFile(ormconfig, data.toString()).catch((err) => {
+    const yml = YAML.dump(JSON.parse(json));
+    await writeFile(ormconfig, yml).catch((err) => {
       console.log(err);
       process.exit();
     });
